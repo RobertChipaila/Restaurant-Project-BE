5a9da92fe61dee9b82d8c2df2c0de5b1bcf5cc85
FlaviusGorgan <gorganflavius@gmail.com>
2021-04-18 10:15:25 +0300

    Inital Commit w Spring

---
+++ src/test/java/ro/sdaboys/restaurantapp/RestaurantAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sdaboys.restaurantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
